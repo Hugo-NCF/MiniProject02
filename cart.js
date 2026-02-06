4b7340381508258376235bf447f1cb8e2637fff2
hugocruz <hugopcruz2006@icloud.com>
2026-02-06 15:44:09 -0500

    Css, Js Files

--- cart.js
+++ cart.js
@@ -1,22 +1,43 @@
 // add the default cart screen say your cart is empty
-function createBox() {
-    // 1. Create a new div element
-    const newDiv = document.createElement("div");
 
-    // 2. Set the content and style
-    newDiv.innerHTML = "Hello! I am a dynamically created box.";
-    newDiv.style.width = "200px";
-    newDiv.style.height = "100px";
-    newDiv.style.backgroundColor = "lightblue";
-    newDiv.style.border = "2px solid black";
-    newDiv.style.padding = "10px";
-    newDiv.style.marginTop = "10px";
+let cart = []; // initialize an empty cart
 
-    // 3. Append the new element to the body
-    document.body.appendChild(newDiv);
+
+// Function to display the cart screen (when pressing the cart button)
+function displayCart() {
+    document.getElementById("cart").classList.add("open");
+    document.getElementById("cart-overlay").classList.add("open");
 }
-/* when press add to cart, have the cart screen pop up 
-from the right side covering 1/5 of the page */
 
-/* on cart screen have a delete button, and an add more button, 
-and a checkout button with an alert that says "Purchased Items!" */
+function closeCart() {
+    document.getElementById("cart").classList.remove("open");
+    document.getElementById("cart-overlay").classList.remove("open");
+}
+
+
+// // Function to add an item to the cart (when pressing add to cart button)
+// function addtoCart(name, price, image) {
+//     let item = {
+//         name: name,
+//         price: price,
+//         image: image
+//     };
+//     cart.push(item); // add the item to the cart array
+//     console.log(cart); // log the current state of the cart
+// }
+
+// // Function to delete an item from the cart (when pressing delete button in cart sidebar)
+// function clearCart(index) {
+//     if (index >= 0 && index < cart.length) {
+//         cart.splice(index, 1); // remove the item at the specified index
+//         console.log("Item removed from cart");
+//     } else {
+//         console.log("Invalid index");
+//     }
+// }
+
+// /* when press add to cart, have the cart screen pop up 
+// from the right side covering 1/5 of the page */
+
+// /* on cart screen have a delete button, and an add more button, 
+// and a checkout button with an alert that says "Purchased Items!" */
